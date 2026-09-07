a369bd670637db4e9d586f2b264f06206202ac4d
L.W. <reaktor.utfallen-2z@icloud.com>
2026-09-07 14:25:56 +0200

    Add useState-based input fields for task creation(title, description, assignee, category, priority, status)

---
+++ task-board/src/components/NewTaskForm.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import type {Category, Priority, TaskStatus } from "../types/Task";
+
+
+const NewTaskForm = ()=>{
+
+
+const [title, setTitle] = useState("");
+const [description, setDescription] =useState("");
+const [assignee, setAssignee] = useState("");
+const [category, setCategory] =useState<Category>("Frontend");
+const [priority, setPriority] =useState<Priority>("Medium");
+const [status, setStatus] =useState<TaskStatus>("Todo");
+
+return(
+    <>
+    <input 
+    value={title}
+    onChange={(event) => setTitle(event.target.value)} />;
+
+    <textarea
+    value={description} 
+    onChange={(event) =>setDescription(event.target.value)}/>;
+
+
+    <input 
+    value={assignee}
+    onChange={(event)=> setAssignee(event.target.value)} />;
+
+    <select
+    value={category}
+    onChange={(event) => setCategory(event.target.value as Category)}> 
+    <option value="Frontend">Frontend</option>
+    <option value="Backend">Backend</option>
+    <option value="Test">Test</option>
+    <option value="Design">Design</option>
+    <option value="API">API</option>
+    </select>;
+
+    <select
+    value={priority}
+    onChange={(event) => setPriority(event.target.value as Priority)}>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+    </select>;
+
+
+
+    <select
+    value={status}
+    onChange={(event)=> setStatus(event.target.value as TaskStatus)}>
+        <option value="Todo">Todo</option>
+        <option value="Doing">Doing</option>
+        <option value="Done">Done</option>
+    </select>;
+
+    </>
+);
+
+
+};
+
+export default NewTaskForm;
+
+
