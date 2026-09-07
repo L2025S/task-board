a9a0945d7252cdf582b9e001dd8337ce28783d99
L.W. <reaktor.utfallen-2z@icloud.com>
2026-09-07 21:24:11 +0200

    Apply Tailwind CSS styling to Fotter.tsx

--- task-board/src/components/Footer.tsx
+++ task-board/src/components/Footer.tsx
@@ -6,7 +6,11 @@ type FooterProps ={
 const Footer =({creator,year}:FooterProps)=>{
 
     return(
-        <footer>&copy; created by {creator} in {year}</footer>
+        <footer className="bg-[#f9dcc4] text-black text-center py-4 mt-auto border-t border-[#e8c4b0]">
+        <p className="text-sm opacity-80"> 
+            &copy; created by {creator} in {year}
+        </p>
+        </footer>
     );
 };
 export default Footer;
