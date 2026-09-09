e1795d2be534139f9e11e3e2465b746197c4b9dd
L.W. <reaktor.utfallen-2z@icloud.com>
2026-09-09 19:33:54 +0200

    Load tasks from backend on app initialization.

--- task-board/src/App.tsx
+++ task-board/src/App.tsx
@@ -3,122 +3,214 @@ import Footer from "./components/Footer";
 import TaskCard from "./components/TaskCard";
 import type { Task, NewTask } from "./types/Task";
 import NewTaskForm from "./components/NewTaskForm";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+
+const API_URL ="http://localhost:3001/api/tasks";
 
 
-const initialTasks: Task[] = [
-  {
-    id: 1,
-    title: "Implement Authentication",
-    description: " Add JWT-based login and registration flow.",
-    assignee: "Alice",
-    category: "Backend",
-    priority: "High",
-    status: "Todo",
-  },
-  {
-    id: 2,
-    title: "Create UI Layout",
-    description: "Design the main dashboard layout using React and Tailwind.",
-    assignee: "Bob",
-    category: "Frontend",
-    priority: "Medium",
-    status: "Todo",
-  },
-  {
-    id: 3,
-    title: "Write Unit Tests",
-    description: "Add Jest tests for core utility functions.",
-    assignee: "Charlie",
-    category: "Test",
-    priority: "Low",
-    status: "Todo",
-  },
-  {
-    id: 4,
-    title: "Build API Endpoints",
-    description: "Connect CRUD endpoints for project resources.",
-    assignee: "Diana",
-    category: "Backend",
-    priority: "High",
-    status: "Doing",
-  },
-  {
-    id: 5,
-    title: "Integrate Payment Gateway",
-    description: "Connect Stripe API and handle payment flow.",
-    assignee: "Evan",
-    category: "API",
-    priority: "High",
-    status: "Doing",
-  },
-  {
-    id: 6,
-    title: "Build Resuable Components",
-    description: "Create shared UI components like buttons, modals, and forms.",
-    assignee: "Fiona",
-    category: "Frontend",
-    priority: "Medium",
-    status: "Doing",
-  },
-  {
-    id: 7,
-    title: "Set Up Project Structure",
-    description:
-      "Initialize repository, configure TypeScript, ESLint, and Prettier.",
-    assignee: "George",
-    category: "Backend",
-    priority: "Medium",
-    status: "Done",
-  },
-  {
-    id: 8,
-    title: "Configure CI/CD",
-    description:
-      "Add GitHub Actions workflow for automated testing and deployment.",
-    assignee: "Hannah",
-    category: "Backend",
-    priority: "High",
-    status: "Done",
-  },
-  {
-    id: 9,
-    title: "Implement Logging",
-    description: "Add Winston-based logging with error tracking.",
-    assignee: "Ian",
-    category: "Backend",
-    priority: "Low",
-    status: "Done",
-  },
-];
+
+// const initialTasks: Task[] = [
+//   {
+//     id: 1,
+//     title: "Implement Authentication",
+//     description: " Add JWT-based login and registration flow.",
+//     assignee: "Alice",
+//     category: "Backend",
+//     priority: "High",
+//     status: "Todo",
+//   },
+//   {
+//     id: 2,
+//     title: "Create UI Layout",
+//     description: "Design the main dashboard layout using React and Tailwind.",
+//     assignee: "Bob",
+//     category: "Frontend",
+//     priority: "Medium",
+//     status: "Todo",
+//   },
+//   {
+//     id: 3,
+//     title: "Write Unit Tests",
+//     description: "Add Jest tests for core utility functions.",
+//     assignee: "Charlie",
+//     category: "Test",
+//     priority: "Low",
+//     status: "Todo",
+//   },
+//   {
+//     id: 4,
+//     title: "Build API Endpoints",
+//     description: "Connect CRUD endpoints for project resources.",
+//     assignee: "Diana",
+//     category: "Backend",
+//     priority: "High",
+//     status: "Doing",
+//   },
+//   {
+//     id: 5,
+//     title: "Integrate Payment Gateway",
+//     description: "Connect Stripe API and handle payment flow.",
+//     assignee: "Evan",
+//     category: "API",
+//     priority: "High",
+//     status: "Doing",
+//   },
+//   {
+//     id: 6,
+//     title: "Build Resuable Components",
+//     description: "Create shared UI components like buttons, modals, and forms.",
+//     assignee: "Fiona",
+//     category: "Frontend",
+//     priority: "Medium",
+//     status: "Doing",
+//   },
+//   {
+//     id: 7,
+//     title: "Set Up Project Structure",
+//     description:
+//       "Initialize repository, configure TypeScript, ESLint, and Prettier.",
+//     assignee: "George",
+//     category: "Backend",
+//     priority: "Medium",
+//     status: "Done",
+//   },
+//   {
+//     id: 8,
+//     title: "Configure CI/CD",
+//     description:
+//       "Add GitHub Actions workflow for automated testing and deployment.",
+//     assignee: "Hannah",
+//     category: "Backend",
+//     priority: "High",
+//     status: "Done",
+//   },
+//   {
+//     id: 9,
+//     title: "Implement Logging",
+//     description: "Add Winston-based logging with error tracking.",
+//     assignee: "Ian",
+//     category: "Backend",
+//     priority: "Low",
+//     status: "Done",
+//   },
+// ];
 
 const App = () => {
 
-  // Use useState to manage tasks
-  const [tasks, setTasks]=useState<Task[]>(initialTasks);
+  // Use useState to manage tasks 
+  const [tasks, setTasks]=useState<Task[]>([]);
+  const  [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null> (null);
 
-  const handleAddTask = (newTask: NewTask )=> {
+ // =======================  Load data from backend ===================
+
+ const fetchTasks = async () =>{
+      try{
+        setLoading(true);
+        const response = await fetch(API_URL);
+
+        if(!response.ok){
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setTasks(data);
+        setError(null);
+      } catch (err){
+        setError( err instanceof Error ? err.message :"Failed to load data.");
+        console.error("Failed to fetch tasks:", err);
+      } finally {
+        setLoading(false);
+      }
+ };
+
+ // Fetch data when the component loads
+
+ useEffect(() =>{
+  fetchTasks();
+ }, []);
+
+
+ // ================== Add New Tasks ==================================================
+
+ const handleAddTask = async (newTask: NewTask) =>{
+      try{
+        const response = await fetch (API_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newTask),
+        });
+
+        if (!response.ok){
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const savedTask = await response.json();
+
+        // Update tasks
+        setTasks([...tasks, savedTask]);
+        console.log("A new task is added:", savedTask);
+      } catch (err){
+        console.error("Failed to add task: ", err);
+        alert("Failed to add task, please try again later.");
+      }
+ };
+ 
+  //const handleAddTask = (newTask: NewTask )=> {
     // generate new Id 
-    const maxId = tasks.reduce((max, task) =>(task.id && task.id > max? task.id: max), 0);
+   // const maxId = tasks.reduce((max, task) =>(task.id && task.id > max? task.id: max), 0);
     
-    const newTaskWithId: Task = {
-      ...newTask,
-      id: maxId +1,
-      status:"Todo",
-    };
+   // const newTaskWithId: Task = {
+    //  ...newTask,
+   //   id: maxId +1,
+   //   status:"Todo",
+   // };
 
-    setTasks([...tasks, newTaskWithId]);
+   // setTasks([...tasks, newTaskWithId]);
 
-    console.log("Updated tasks:",[...tasks, newTaskWithId]);
-  }
+   // console.log("Updated tasks:",[...tasks, newTaskWithId]);
+  //}
 
   
 
 
-  // Filter Todo
+  // Filter tasks
   const todoTasks = tasks.filter((task) => task.status === "Todo");
   const doingTasks = tasks.filter((task) => task.status === "Doing");
   const doneTasks = tasks.filter((task) => task.status === "Done");
+
+  // Loading
+  
+  if(loading){
+    return(
+      <div className="min-h-screen bg-[#f8edeb] flex items-center justify-center">
+        <p className="text-xl text-gray-600">Loading...</p>
+      </div>
+
+    );
+  }
+
+  
+  // Error while loading
+
+  if(error){
+    return(
+      <div className="min-h-screen bg-[#f8edeb] flex items-center justify-center">
+          <div className="text-center">
+            <p className="text-xl text-red-600">❌ Failed to load</p>
+            <p className="text-gray-600">{error}</p>
+            <button 
+            onClick={fetchTasks}
+            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              Try Again
+              </button>
+          </div>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen bg-[#f8edeb] font-sans">
